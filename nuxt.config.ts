2c59c60b17eeb67e61c08521f8de805cd747370c
roymandev <royman.dev@gmail.com>
2023-11-08 06:10:25 +0700

    style: run lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/eslint-module"],
+  modules: ['@nuxt/ui', '@nuxtjs/eslint-module'],
   typescript: {
     typeCheck: true,
-    strict: true,
-  },
-});
+    strict: true
+  }
+})
