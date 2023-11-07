59c9e035e65e40a45c4ffa8a633098e51b121c72
roymandev <royman.dev@gmail.com>
2023-11-08 06:54:18 +0700

    feat: use Rubik font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/eslint-module'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/eslint-module',
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
+  ],
   typescript: {
     typeCheck: true,
     strict: true,
+  },
+  googleFonts: {
+    families: {
+      Rubik: true,
+    },
+    subsets: ['latin'],
+    display: 'swap',
   },
 });
